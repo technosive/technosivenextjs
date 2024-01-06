12c36f3244a2aacd79e9fb5e376e612fe291d699
Prathamesh-technosive <147493795+Prathamesh-technosive@users.noreply.github.com>
2024-01-06 17:13:01 +0530

    fix: Resolved issue with email sending even when inputs are empty in subscriber form

--- components/Contact/NewsLatterBox.tsx
+++ components/Contact/NewsLatterBox.tsx
@@ -98,7 +98,6 @@ const NewsLatterBox = () => {
         />
         <button
           type="submit"
-          onClick={handleFormSubmit}
           className="mb-5 flex w-full cursor-pointer items-center justify-center rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark"
         >
           {loading ? "Subscribing.." : "Subscribe"}
