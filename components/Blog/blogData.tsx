67c75b7b6c7caec983a94fb38c2cbefc7b512de9
Prathamesh-technosive <147493795+Prathamesh-technosive@users.noreply.github.com>
2024-02-15 13:29:44 +0530

    feat: New blog added (Smart Building Monitoring)

--- components/Blog/blogData.tsx
+++ components/Blog/blogData.tsx
@@ -1,9 +1,9 @@
 import { Blog } from "@/types/blog";
 
 //Blog Images
-import blogImg2 from "../../public/images/blog/blog-02.webp";
 import blogImg3 from "../../public/images/blog/blog-03.webp";
 import blogImg1 from "../../public/images/blog/revolutionizing-healthcare/160.webp";
+import blogImg2 from "../../public/images/blog/smart-building-monitoring/160.webp";
 
 // Author Images
 import blogAuthorImg2 from "../../public/images/blog/author-02.webp";
@@ -28,18 +28,18 @@ const blogData: Blog[] = [
   },
   {
     id: 2,
-    title: "9 simple ways to improve your design skills",
-    path: "/blogs/improve-design-skills",
+    title: "Smart Building Monitoring",
+    path: "/blogs/smart-building-monitoring",
     paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
+      "In the realm of technological progress, smart buildings stand out as beacons of innovation, promising enhanced efficiency and sustainability. At the core of this revolution...",
     image: blogImg2,
     author: {
       name: "Musharof Chy",
       image: blogAuthorImg2,
       designation: "Content Writer",
     },
-    tags: ["computer"],
-    publishDate: "2025",
+    tags: ["IOT"],
+    publishDate: "15/02/2024",
   },
   {
     id: 3,
